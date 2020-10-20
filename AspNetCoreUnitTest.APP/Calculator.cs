813194191f11d29c7fe6101a935538642b7f2edc
Mehmet Yağcı <mehmetyagci53@gmail.com>
2020-10-20 09:09:08 +0300

    Initial commit

---
+++ AspNetCoreUnitTest.APP/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCoreUnitTest.APP
+{
+    public class Calculator
+    {
+
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ AspNetCoreUnitTest.Test/CalculatorTest.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCoreUnitTest.APP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AspNetCoreUnitTest.Test
+{
+    public class CalculatorTest
+    {
+
+        [Fact]
+        public void AddTest()
+        {
+            // Arrange -> değişlenlerin initialize edildiği yerdir
+            int a = 5;
+            int b = 20;
+            var calculator = new Calculator();
+
+            // Act -> 
+            var total = calculator.add(a, b);
+
+            // Assert -> doğrulama evresi
+            Assert.Equal<int>(25, total);
+        }
+    }
+}
