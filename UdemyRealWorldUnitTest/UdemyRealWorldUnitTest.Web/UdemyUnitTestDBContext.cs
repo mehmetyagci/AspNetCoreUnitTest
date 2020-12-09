176c6231a9dbefa74883f770e3f168ae04c9a8e1
Mehmet Yağcı <mehmetyagci53@gmail.com>
2020-12-09 23:47:28 +0300

    asd
    ad

--- UdemyRealWorldUnitTest/UdemyRealWorldUnitTest.Web/UdemyUnitTestDBContext.cs
+++ UdemyRealWorldUnitTest/UdemyRealWorldUnitTest.Web/UdemyUnitTestDBContext.cs
@@ -19,11 +19,7 @@ namespace UdemyRealWorldUnitTest.Web
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-MKBEOKP\\MYSQLSERVER;Initial Catalog=UdemyUnitTestDB;Integrated Security=True");
-            }
+           
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
