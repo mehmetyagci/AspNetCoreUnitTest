89b67280cdb085de8535bbbf2582e4c5e56cbf62
Mehmet Yağcı <mehmetyagci53@gmail.com>
2020-12-09 21:55:08 +0300

    asd
    asd

---
+++ UdemyRealWorldUnitTest/UdemyRealWorldUnitTest.Web/UdemyUnitTestDBContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace UdemyRealWorldUnitTest.Web
+{
+    public partial class UdemyUnitTestDBContext : DbContext
+    {
+        public UdemyUnitTestDBContext()
+        {
+        }
+
+        public UdemyUnitTestDBContext(DbContextOptions<UdemyUnitTestDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-MKBEOKP\\MYSQLSERVER;Initial Catalog=UdemyUnitTestDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Color).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(200);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
